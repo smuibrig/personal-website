80acba8d499e01388cfb1b654e5b7cb6ac1bddb1
smuibrig <sophiemulleruibrig@gmail.com>
2020-09-14 19:27:24 +0200

    update

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/sophie/Desktop/personal-website/.cache/dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/sophie/Desktop/personal-website/src/pages/index.js")))
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,31 @@
-import React from "react"
+import React from "react";
 
 export default function Home() {
-  return <div>Hello world!</div>
+    return (
+        <div>
+            <div className="content-wrapper" id="name-title">
+                <h1>Sophie Müller-Uibrig</h1>
+                <h2 id="position">Web Developer</h2>
+                <h3>About</h3>
+                <p className="about-section">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Turpis in eu mi bibendum neque. Vitae nunc sed velit
+                    dignissim sodales ut eu sem. Malesuada fames ac turpis
+                    egestas sed tempus urna et. Amet porttitor eget dolor morbi.
+                    Mauris a diam maecenas sed enim ut sem. Consequat ac felis
+                    donec et odio. Dignissim enim sit amet venenatis urna cursus
+                    eget nunc. Quam id leo in vitae turpis massa sed elementum
+                    tempus. Commodo ullamcorper a lacus vestibulum sed arcu non
+                    odio euismod. Dui nunc mattis enim ut. Dignissim enim sit
+                    amet venenatis urna cursus eget nunc scelerisque. Blandit
+                    cursus risus at ultrices. Leo in vitae turpis massa sed
+                    elementum tempus egestas. Adipiscing elit duis tristique
+                    sollicitudin nibh sit amet. Magna eget est lorem ipsum dolor
+                    sit amet consectetur adipiscing. Est pellentesque elit
+                    ullamcorper dignissim cras tincidunt.
+                </p>
+            </div>
+        </div>
+    );
 }
