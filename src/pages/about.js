2253e1118a42d3a1295bb77ed66c53aac3d36518
smuibrig <sophiemulleruibrig@gmail.com>
2020-09-14 22:28:17 +0200

    add links

---
+++ src/pages/about.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div>
+            <div className="content-wrapper">
+                <p className="about-section">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Turpis in eu mi bibendum neque. Vitae nunc sed velit
+                    dignissim sodales ut eu sem. Malesuada fames ac turpis
+                    egestas sed tempus urna et. Amet porttitor eget dolor morbi.
+                    Mauris a diam maecenas sed enim ut sem. Consequat ac felis
+                    donec et odio. Dignissim enim sit amet venenatis urna cursus
+                    eget nunc. Quam id leo in vitae turpis massa sed elementum
+                    tempus. Commodo ullamcorper a lacus vestibulum sed arcu non
+                    odio euismod. Dui nunc mattis enim ut. Dignissim enim sit
+                    amet venenatis urna cursus eget nunc scelerisque. Blandit
+                    cursus risus at ultrices. Leo in vitae turpis massa sed
+                    elementum tempus egestas. Adipiscing elit duis tristique
+                    sollicitudin nibh sit amet. Magna eget est lorem ipsum dolor
+                    sit amet consectetur adipiscing. Est pellentesque elit
+                    ullamcorper dignissim cras tincidunt.
+                </p>
+            </div>
+        </div>
+    );
+}
