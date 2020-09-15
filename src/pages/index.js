f4e9a756e6eb31606c6cbf9a0d6ad2af155790b4
smuibrig <sophiemulleruibrig@gmail.com>
2020-09-15 21:41:48 +0200

    add styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,21 @@ import { Link } from "gatsby";
 
 export default function Home() {
     return (
-        <div>
+        <div className="content-wrapper" id="center">
             <div className="content-wrapper" id="name-title">
                 <h1>Sophie Müller-Uibrig</h1>
                 <h2 id="position">Junior Web Developer</h2>
             </div>
-            <div id="circle"></div>
+            <div id="circle-wrapper">
+                <div id="circle"></div>
+            </div>
+            <div id="short-bio">
+                Hi! I'm Sophie a Junior Web Developer with a background in
+                Linguistics. Frameworks and libraries I currently enjoy working
+                with are: React.js, Gatsby, Vue.js, jQuery, Handlebars,
+                Node.js/Express, Jest. I'm also in the process of learning
+                TypeScript.
+            </div>
             <div className="links-wrapper">
                 <Link to="/about/">about</Link>
                 <a
@@ -24,6 +33,7 @@ export default function Home() {
                 <a href="https://www.linkedin.com/in/sophiemulleruibrig/">
                     LinkedIn
                 </a>
+                <a href="mailto:sophiemulleruibrig@gmail.com">mail</a>
             </div>
         </div>
     );
