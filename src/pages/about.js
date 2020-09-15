111e641f9cc01b91c9e0764767ccc0b4b6da3d10
smuibrig <sophiemulleruibrig@gmail.com>
2020-09-15 23:27:16 +0200

    Edit about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,32 +4,45 @@ export default function About() {
     return (
         <div>
             <div className="content-wrapper">
-                <h3>How a became a programmer</h3>
+                <h3>About</h3>
                 <p className="about-section">
-                    My interest in programming first sparked while finishing my
-                    MA in Communications and Linguistics in 2019, when I
-                    enrolled in a Computational Linguistics course. The course
-                    entailed natural language processing with python. Back then
-                    I wrote my first lines of code and got a first hunch of how powerful a tool this is.
+                    My interest in coding first sparked while finishing my MA in
+                    Communications and Linguistics in 2019, when I enrolled in a
+                    Computational Linguistics course. It entailed natural
+                    language processing with python. Back then I wrote my first
+                    lines of code and got a hunch of how powerful a tool
+                    programming is.
                 </p>
                 <p className="about-section">
                     After gaining some professional experience in Communications
-                    and Marketing in the Berlin tech sector, I decided, that I
+                    and Marketing in the Berlin tech sector, I decided that I
                     would like to get my hands dirty and build technologies,
                     rather than market them.
                 </p>
                 <p className="about-section">
                     So in the beginning of 2020 I quit my job to fully commit to
-                    learn how to build web apps. As a linguist I put careful
-                    consideration into which language to focus on at this stage.
-                    JavaScript made the cut since it is used to build the web's
-                    UI and it's also a valid backend language.
+                    learn how to program and build web apps. As a linguist I put
+                    careful consideration into which language to focus on at
+                    this stage. JavaScript made the cut: is one of the core
+                    technologies of the web, it is the only programming language
+                    that enables interactive web pages. With the help of
+                    cross-platform runtime engines like Node.js also server-side
+                    code can be written in JavaScript.
                 </p>
                 <p className="about-section">
-                    Since Web Development comprises of a number of different frameworks and libraries and offers countless resources to learn them, I finally came to a point where sought guidance in my very personal study endevour. So I did my research and decided to enroll at SPICED Academy, an intense three months WebDev bootcamp based in Berlin.
+                    Since Web Development comprises a number of different
+                    frameworks and libraries and offers countless resources to
+                    learn them, I finally came to a point where I sought
+                    guidance in my very personal study endeavour. So I did my
+                    research and decided to enroll at SPICED Academy, an intense
+                    three months WebDev bootcamp based in Berlin.
                 </p>
                 <p className="about-section">
-                    After building my first full stack web apps, working with third party APIs and using frontend frameworks such as React.js, Vue.js and Gatsby, I'm ready to learn more on a job from exprienced developers, who enjoy to mentor an avid learner like myself.   
+                    After building my first full stack web apps, working with
+                    third party APIs and using frontend frameworks such as
+                    React.js, Vue.js and Gatsby I'm ready to learn more on a job
+                    from experienced developers, who enjoy to mentor an avid
+                    learner like myself.
                 </p>
             </div>
         </div>
