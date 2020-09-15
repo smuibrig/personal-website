7e46b2250925361a459155304fd71893485c7d48
smuibrig <sophiemulleruibrig@gmail.com>
2020-09-15 23:03:59 +0200

    add bout me text

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,23 +4,32 @@ export default function About() {
     return (
         <div>
             <div className="content-wrapper">
+                <h3>How a became a programmer</h3>
                 <p className="about-section">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Turpis in eu mi bibendum neque. Vitae nunc sed velit
-                    dignissim sodales ut eu sem. Malesuada fames ac turpis
-                    egestas sed tempus urna et. Amet porttitor eget dolor morbi.
-                    Mauris a diam maecenas sed enim ut sem. Consequat ac felis
-                    donec et odio. Dignissim enim sit amet venenatis urna cursus
-                    eget nunc. Quam id leo in vitae turpis massa sed elementum
-                    tempus. Commodo ullamcorper a lacus vestibulum sed arcu non
-                    odio euismod. Dui nunc mattis enim ut. Dignissim enim sit
-                    amet venenatis urna cursus eget nunc scelerisque. Blandit
-                    cursus risus at ultrices. Leo in vitae turpis massa sed
-                    elementum tempus egestas. Adipiscing elit duis tristique
-                    sollicitudin nibh sit amet. Magna eget est lorem ipsum dolor
-                    sit amet consectetur adipiscing. Est pellentesque elit
-                    ullamcorper dignissim cras tincidunt.
+                    My interest in programming first sparked while finishing my
+                    MA in Communications and Linguistics in 2019, when I
+                    enrolled in a Computational Linguistics course. The course
+                    entailed natural language processing with python. Back then
+                    I wrote my first lines of code and got a first hunch of how powerful a tool this is.
+                </p>
+                <p className="about-section">
+                    After gaining some professional experience in Communications
+                    and Marketing in the Berlin tech sector, I decided, that I
+                    would like to get my hands dirty and build technologies,
+                    rather than market them.
+                </p>
+                <p className="about-section">
+                    So in the beginning of 2020 I quit my job to fully commit to
+                    learn how to build web apps. As a linguist I put careful
+                    consideration into which language to focus on at this stage.
+                    JavaScript made the cut since it is used to build the web's
+                    UI and it's also a valid backend language.
+                </p>
+                <p className="about-section">
+                    Since Web Development comprises of a number of different frameworks and libraries and offers countless resources to learn them, I finally came to a point where sought guidance in my very personal study endevour. So I did my research and decided to enroll at SPICED Academy, an intense three months WebDev bootcamp based in Berlin.
+                </p>
+                <p className="about-section">
+                    After building my first full stack web apps, working with third party APIs and using frontend frameworks such as React.js, Vue.js and Gatsby, I'm ready to learn more on a job from exprienced developers, who enjoy to mentor an avid learner like myself.   
                 </p>
             </div>
         </div>
